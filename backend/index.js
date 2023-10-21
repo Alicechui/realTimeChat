02477437cfd91aa3b9972a039ab7b160639a65e5
AliceChui <alicechuihy@outlook.com>
2023-10-21 04:29:56 +0200

    react project created

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,24 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "e1fc038e-e104-4ccf-93b3-bc2615dddddc" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
   return res.json({ username: username, secret: "sha256..." });
 });
 
